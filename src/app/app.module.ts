76c758ebfcf9a40d31ba9e0830e314ac8d24aeeb
Urbanz91 <43339296+Urbanz91@users.noreply.github.com>
2020-05-27 18:56:17 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { JexcelTableComponent } from './jexcel-table/jexcel-table.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, JexcelTableComponent ],
+  declarations: [ AppComponent, JexcelTableComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
