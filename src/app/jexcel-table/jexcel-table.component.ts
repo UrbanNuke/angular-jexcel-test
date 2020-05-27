681403867205c47f5374f2820f1442a97a2a0799
Urbanz91 <turbino4ka91@gmail.com>
2020-05-27 18:09:16 +0300

    jexcel-table

---
+++ src/app/jexcel-table/jexcel-table.component.ts
@@ -0,0 +1,45 @@
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
+import * as jexcel from 'jexcel';
+
+@Component({
+  selector: 'app-jexcel-table',
+  templateUrl: './jexcel-table.component.html',
+  styleUrls: ['./jexcel-table.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class JexcelTableComponent implements AfterViewInit {
+
+ 	/** Ссылка на таблицу в шаблоне */
+	@ViewChild('spreadsheet') spreadsheet: ElementRef;
+
+	/** Путь до файла */
+	urlFile = 'jexcel-example.xlsx';
+
+	ngAfterViewInit() {
+		// @ts-ignore
+		jexcel.fromSpreadsheet(this.urlFile, result => {
+			if (!result.length) {
+				console.error('JEXCEL: Something went wrong.');
+				return;
+			}
+
+			jexcel(this.spreadsheet.nativeElement, {
+				data:  result[0].data,
+				columns:  result[0].columns.map(() => ({
+					align: 'left',
+					width: 280,
+					wordWrap: true,
+				})),
+				tableWidth: '100%',
+				tableHeight: '100%',
+				tableOverflow: true,
+				lazyLoading: true,
+				loadingSpin: true,
+				contextMenu: () => false,
+				columnSorting: false,
+				minDimensions: [result[0].columns.length, result[0].rows.length]
+			});
+		});
+	}
+
+}
--- src/main.ts
+++ src/main.ts
@@ -14,3 +14,12 @@ platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
 
   // Otherwise, log the boot error
 }).catch(err => console.error(err));
+
+const addScript = link => {
+  const script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = link;
+  document.head.append(script);
+}
+
+addScript("./node_modules/xlsx/dist/xlsx.extendscript.js");
